55296fcaccc46ecc826e71d7441a0d9a45830bfc
Gabriel Imenes <69609443+imenesg@users.noreply.github.com>
2026-02-12 23:35:19 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import Hero from './components/Hero'
+import ExperienceList from "./components/ExperienceCards";
+import ProjectCards from "./components/ProjectsCards";
+import Footer from "./components/Footer";
+
+function App() {
+
+    return (
+        <>
+            <Hero></Hero>
+            <ExperienceList></ExperienceList>
+            <ProjectCards></ProjectCards>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
